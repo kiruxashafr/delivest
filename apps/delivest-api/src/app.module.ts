bda10e1e90d0a88d76550225b28cc9a19d4f9a75
kiruxashafr <kirshafranov@gmail.com>
2026-03-21 18:01:20 +0300

    fix: copy shared to src

--- apps/delivest-api/src/app.module.ts
+++ apps/delivest-api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { NetModule } from './net/net.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '../../.env' }),
     PrismaModule,
+    NetModule,
   ],
   controllers: [],
   providers: [],
