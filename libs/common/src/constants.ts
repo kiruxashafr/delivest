3e2a2e62275bdf85e02a61b8e4a838955cbc20b4
kiruxashafr <kirshafranov@gmail.com>
2026-04-19 16:04:00 +0300

    format

--- libs/common/src/constants.ts
+++ libs/common/src/constants.ts
@@ -3,7 +3,7 @@ export const PHONE_REGEX = /^\+7\d{10}$/;
 export const MIN_NAME_LENGTH = 1;
 export const MAX_NAME_LENGTH = 100;
 export const COOKIE_NAMES = {
-  CLIENT_REFRESH_TOKEN: 'client_refresh_token',
-  STAFF_REFRESH_TOKEN: 'staff_refresh_token', 
-  SESSION_ID: 'session_id',
+  CLIENT_REFRESH_TOKEN: "client_refresh_token",
+  STAFF_REFRESH_TOKEN: "staff_refresh_token",
+  SESSION_ID: "session_id",
 } as const;
